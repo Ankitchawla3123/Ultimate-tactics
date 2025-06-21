8a5f84ea5bef52c3cfb8064ddeda67c7014f3792
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-06-21 15:39:26 +0530

    line state array added with mouseup handling

--- src/components/board/BoardStruct.jsx
+++ src/components/board/BoardStruct.jsx
@@ -1,10 +1,22 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getPointerPosition } from "../../utils/getPointerPosition";
 
 function BoardStruct() {
   const boardref = useRef(null);
   const [line, setline] = useState("");
+  const lineRef = useRef(line);
+  const [lines, setlines] = useState([]);
+  const [isDrawn, setisDrawn] = useState(false);
   const [tempPoint, settempPoint] = useState("");
+
+  useEffect(() => {
+    lineRef.current = line;
+  }, [line]);
+
+  useEffect(() => {
+    window.addEventListener("mouseup", handleMouseUp);
+    return () => window.removeEventListener("mouseup", handleMouseUp);
+  }, []);
 
   const drawline = (e) => {
     const { x, y } = getPointerPosition(e, boardref);
@@ -13,15 +25,23 @@ function BoardStruct() {
     } else {
       setline((prev) => ({ ...prev, x2: x, y2: y }));
     }
-    console.log(line);
   };
 
   const Temppointset = (e) => {
-    if (line  && e.buttons ==1 ) {
-      const {x,y} = getPointerPosition(e,boardref);
-      settempPoint({x:x,y:y})
+    if (e.buttons == 1) {
+      setisDrawn(true);
+      drawline(e);
+      const { x, y } = getPointerPosition(e, boardref);
+      settempPoint({ x: x, y: y });
     }
+  };
 
+  const handleMouseUp = (e) => {
+    const currentLine = lineRef.current;
+    if (currentLine !== "") {
+      setlines((prev) => [...prev, currentLine]);
+      setline("");
+    }
   };
 
   return (
@@ -30,10 +50,7 @@ function BoardStruct() {
         ref={boardref}
         width="100%"
         height="100%"
-        onClick={(e) => {
-          drawline(e);
-        }}
-        onMouseMove={(e)=>Temppointset(e)}
+        onMouseMove={(e) => Temppointset(e)}
       >
         {line && (
           <line
@@ -47,6 +64,19 @@ function BoardStruct() {
             strokeLinecap="round"
           />
         )}
+        {lines.map((line, i) => (
+          <line
+            key={i}
+            x1={`${line.x1}%`}
+            y1={`${line.y1}%`}
+            x2={`${line.x2}%`}
+            y2={`${line.y2}%`}
+            style={{ cursor: "pointer" }}
+            stroke="black"
+            strokeWidth="5"
+            strokeLinecap="round"
+          />
+        ))}
       </svg>
     </div>
   );
