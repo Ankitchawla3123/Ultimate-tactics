531f79f1bd12f641274ec33da30dcaac0c4d5201
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-08-11 22:42:05 +0530

    route setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ Added for routing
 import { Provider } from "react-redux";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import store from "./store/store.js";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Layout component with <Outlet/>
+    children: [
+      {
+        index: true, // Default route "/"
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
