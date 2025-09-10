f145c1207e8549311ce951361bfbbcbe420bbee5
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-09-10 19:41:32 +0530

    footer added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -158,7 +158,7 @@ export const HeroHeader = () => {
                   size="sm"
                   className={cn(isScrolled ? "lg:inline-flex" : "hidden")}
                 >
-                  <Link to="/login">
+                  <Link to="/">
                     <span>Get Started</span>
                   </Link>
                 </Button>
